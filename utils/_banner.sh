a0f868025dfb3ee313952ac9ec1e9f44867b9f77
Gleicilene <156491462+gleicilenedias@users.noreply.github.com>
2024-03-21 20:24:31 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -25,11 +25,11 @@ printf "${CYAN_LIGHT}";
 
 
 printf ${CYAN_LIGHT}" ######      #########     ###       #####\n";
-printf ${CYAN_LIGHT}"  ##  ##         ###     ##  ##     ##  ##\n";
-printf ${CYAN_LIGHT}"  ##  ##        ###     ##   ##     ##   ## \n";
+printf ${CYAN_LIGHT}"  ##  ##         ###     ##  ##     ##   ##\n";
+printf ${CYAN_LIGHT}"  ##  ##        ###     ##    ##    ##   ## \n";
 printf ${CYAN_LIGHT}"  #####       ###      ##     ##    ##   ## \n";
 printf ${CYAN_LIGHT}"  ##  ##     ###      ##########    ##### \n";
-printf ${CYAN_LIGHT}"  ##  ##    ###      ###      ##    ##**\n";
+printf ${CYAN_LIGHT}"  ##  ##    ###      ###      ##    ##\n";
 printf ${CYAN_LIGHT}" ######    #######  ###       ##    ##\n";
                                                                                                                                                          
   printf "            \033[1;33m        © BigZap - Tecnologia Em Antendimento\n";
